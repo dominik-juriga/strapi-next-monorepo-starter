78c09e9edd4219fd74321de64ed14ecac78c2a8f
Dominik Juriga <145474492+dominik-juriga@users.noreply.github.com>
2026-03-13 14:25:15 +0100

    chore: add env vars

--- apps/docs/docusaurus.config.ts
+++ apps/docs/docusaurus.config.ts
@@ -2,8 +2,8 @@ import { themes as prismThemes } from "prism-react-renderer"
 import type { Config } from "@docusaurus/types"
 import type * as Preset from "@docusaurus/preset-classic"
 
-const url = "https://dominik-juriga.github.io"
-const baseUrl = "/strapi-next-monorepo-starter/"
+const url = process.env.DOCUSAURUS_URL ??"https://dominik-juriga.github.io"
+const baseUrl = process.env.DOCUSAURUS_BASE_URL ??"/strapi-next-monorepo-starter/"
 
 const config: Config = {
   plugins: [
